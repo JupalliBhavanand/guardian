89a1cde1dbed224e08f81162b8096c32789e9b3f
jupalli Bhavanand <156802713+JupalliBhavanand@users.noreply.github.com>
2024-10-17 21:22:58 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,30 +1,4 @@
-{
-    "version": 2,
-    "builds": [
-        {
-            "src": "consultency/wsgi.py",
-            "use": "@vercel/python",
-            "config": { 
-                "maxLambdaSize": "15mb", 
-                "runtime": "python3.9"
-            }
-        },
-        {
-            "src": "build_files.sh",
-            "use": "@vercel/static-build",
-            "config": {
-                "distDir": "staticfiles_build"
-            }
-        }
-    ],
-    "routes": [
-        {
-            "src": "/static/(.*)",
-            "dest": "/static/$1"
-        },
-        {
-            "src": "/(.*)",
-            "dest": "consultency/wsgi.py"
-        }
-    ]
-}
+echo "BUILD START"
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
+echo "BUILD END"
